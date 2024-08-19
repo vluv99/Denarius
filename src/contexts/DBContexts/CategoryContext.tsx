d359d372d97f4739d7987c3b259f0fdc0596536b
vluv99 <v.luva99@gmail.com>
2024-08-19 11:40:59 +0200

    DEN-14 Fix for Categories loading from DB
    On page reload, the categories could return empty array while loading which was casing issues, so now it has a loading screen while it's in progress.

--- src/contexts/DBContexts/CategoryContext.tsx
+++ src/contexts/DBContexts/CategoryContext.tsx
@@ -1,10 +1,16 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Category } from "../../models/CategoryModel";
 import { ChildrenProp } from "../../utils/types";
 import { useGetCategoryData } from "../../hooks/categoryHooks";
 
+export type CategoryCtx = {
+  categories: Category[] | undefined;
+  catLoading: boolean;
+};
+const defCategoryCtx = { categories: undefined, catLoading: true };
+
 // create context
-const CategoryContext = createContext<Category[]>([]);
+const CategoryContext = createContext<CategoryCtx>(defCategoryCtx);
 
 /**
  * Function body of the React context provider element
@@ -12,10 +18,19 @@ const CategoryContext = createContext<Category[]>([]);
  * @constructor
  */
 export function CategoryContextProvider({ children }: ChildrenProp) {
-  const categories: Category[] = useGetCategoryData();
+  const categories: Category[] | undefined = useGetCategoryData();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (categories === undefined) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [categories]);
 
   return (
-    <CategoryContext.Provider value={categories}>
+    <CategoryContext.Provider value={{ categories, catLoading: loading }}>
       {children}
     </CategoryContext.Provider>
   );
@@ -24,6 +39,6 @@ export function CategoryContextProvider({ children }: ChildrenProp) {
 /**
  * Return categories collected from DB with a simplified function
  */
-export const useCategoryContext = (): Category[] => {
+export const useCategoryContext = (): CategoryCtx => {
   return useContext(CategoryContext);
 };
